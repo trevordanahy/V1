93a933262820364bf3847202e05a9d2ae1d5f144
trevordanahy <trevor.danahy@protonmail.com>
2021-10-14 20:10:11 -0600

    Navbar fully functional.  added mobile menu styles.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,47 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import { useState } from 'react'
 import * as styles from '../styles/navbar.module.css'
 
 function Navbar() {
+
+    //Hamburger bttn visibility is controlled in css, 
+    //mobile nav is controled with useState
+    const [isMobile, setIsMobile] = useState(false)
+
+
     return (
         <header>
             <nav>
-                <div>
+                <div classname={styles.brand}>
                     <h1>td.dev</h1>
                 </div>
-                <div className={styles.navRight}>
-                    <div className={styles.links}>
-                        <Link to="#portfolio">Portfolio</Link>
-                        <Link to="#about">About</Link>
-                        <Link to="#experience">Experience</Link>
-                        <Link to="#contact">Contact</Link>
-                    </div>
-                    <button>Mobile Menu</button>
+                <button
+                    className={styles.hamburgerMenu}
+                    onClick={() => setIsMobile(!isMobile)}
+                >
+                    <span className={styles.bar}></span>
+                    <span className={styles.bar}></span>
+                    <span className={styles.bar}></span>
+                </button>
+                <div
+                    className={`
+                    ${styles.navLinks} 
+                    ${isMobile ? styles.showMobile : null}
+                    `}
+                >
+                    <Link to="/#portfolio" onClick={() => setIsMobile(!isMobile)}>
+                        Portfolio
+                    </Link>
+                    <Link to="/#about" onClick={() => setIsMobile(!isMobile)} >
+                        About
+                    </Link>
+                    <Link to="/#experience" onClick={() => setIsMobile(!isMobile)} >
+                        Experience
+                    </Link>
+                    <Link to="/#contact" onClick={() => setIsMobile(!isMobile)}>
+                        Contact
+                    </Link>
                 </div>
             </nav>
         </header >
